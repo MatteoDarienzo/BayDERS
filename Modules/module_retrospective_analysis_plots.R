769bb92cc3e6ef8dbc4b2d88e1bfcf8eca7d202a
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2022-09-09 11:33:04 +0200

    I realized that you have removed these lines because I had a old version and there are differents. However, I think that it is necessary to plot the gauging in the stage record. I added it because it makes sens for me.

--- Modules/module_retrospective_analysis_plots.R
+++ Modules/module_retrospective_analysis_plots.R
@@ -98,6 +98,9 @@ plot.time.shifts.step1 <- function(dir,
   }
   
   t.plot <- t.plot + 
+    geom_point(data=df.RC.step1, aes(x = t , y= h), size = 6, pch =21, fill= df.RC.step1$color) +
+    scale_y_continuous(name   = limni.labels[2], expand = c(0,0), limits = c(grid_limni.ylim[1], grid_limni.ylim[2]), 
+                       breaks = seq(grid_limni.ylim[1], grid_limni.ylim[2], grid_limni.ylim[3]))+
     scale_y_continuous(name   = limni.labels[2], expand = c(0,0), limits = c(grid_limni.ylim[1], grid_limni.ylim[2]), 
                        breaks = seq(grid_limni.ylim[1], grid_limni.ylim[2], grid_limni.ylim[3]))+
     ylab(limni.labels[2])+
