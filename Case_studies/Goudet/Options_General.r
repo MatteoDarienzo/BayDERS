36906eeb36b891dd9a649edfa127b4facd296ae5
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2022-11-25 14:45:03 +0100

    New case of study : La Loire river at Goudet

---
+++ Case_studies/Goudet/Options_General.r
@@ -0,0 +1,133 @@
+###############################################################################################
+#                ==>    Insert all general inputs for the computation   <==                   #
+###############################################################################################
+#Hydrometric station general information:
+#****************************************
+station.name         = "La Loire River at Goudet, France"  # Insert general info of the station (for plots)   
+data.period          = "01/2018 - 06/2022"                 # Insert the study period (for plots)
+
+
+# Gaugings:
+#**********
+file_gaugings        = "Goudet_gauging_2018_2022.csv" # name of the csv file with the gaugings (put FALSE if not available!)
+u.m.Hgaug            = "cm"                     # options: "m", "cm", "mm" or "ft"
+u.m.Qgaug            = "m^3*s^-1"              # options: "m^3.s-1" or "ft^3.s-1"
+QGaug.col            = 2                       # column position in the file ...
+uQGaug.col           = 3                       # column position in the file ...
+hGaug.col            = 1                       # column position in the file ...
+tGaug.col            = 4                      # or c(1,2,3,4,5,6) if a date format in multiple columns.
+tGaug.format         = "%d/%m/%Y %H:%M"        # insert "numeric" or the date format like "%d/%m/%Y %H:%M".  
+uQ.absolute          = FALSE                   # TRUE (if st dev of uncertainty) or FALSE (if % of Q)
+gaugings_filter      = 1                       # consider a gauging every "gaugings_filter" gaugings. 
+
+#settings for RC plot:
+grid_RC.xlim         = c(0,5)                    # grid for RC plots (stage h limits)
+grid_RC.ylim         = c(0,300)                   # grid for RC plots (discharge Q limits)
+grid_RC.xstep        = 1                          # grid for RC plots 
+grid_RC.ystep        = 20                         # grid for RC plots
+grid_RC.ylim.log     = c(0.5, 1000)              # grid for RC plots in y-log scale
+ticks_RC.y.log       = c(0.5,1,10,100,1000) # grid for RC-log plots
+RC.y.labels          = "Discharge Q"              # RC:  y label (for plots)
+RC.x.labels          = "Stage h"                  # RC:  x label (for plots)
+
+
+
+
+
+# Stage record:
+#**************
+file_limni           = "Limnigramme_goudet_2018_2022.csv"              # name of the data .csv file with the stage record (put FALSE if not available!)
+hLimni.col           = 2                                  # column position in the file ...
+tLimni.col           = 1                                  # column position in the file ...
+tLimni.format        = "%d/%m/%Y %H:%M"                   # insert "numeric" or the date format like "%d/%m/%Y %H:%M".  
+limni_filter         = 1                                  # consider a stage value every "limni_filter" values. 
+u.m.limni            = "m"                               # ["m" / "cm" / "mm" / "ft"] unity of the stage record
+grid_limni.ylim      = c(0,5,1)                          # grid for Limni plots
+limni.labels         = c("Time [days]", "Stage h [m]")    # limni: x and y labels (for plots)
+date_origin          = "1899-12-30"                       # origin date to transform dates from numeric (by default is "1899-12-30").
+
+
+
+
+ 
+# Official dates of rating shifts (or RC update):
+#***********************************************
+official.shift.times = "DateShiftsGoudet.csv"  # name of the csv file with official shift dates (put FALSE if not available!)
+tOfficial.col        = 1                       # column position in the file ...
+tOfficial.format     = "%d/%m/%Y %H:%M"              # insert "numeric" or the date format like "%d/%m/%Y %H:%M".  
+
+
+
+
+
+
+
+
+
+
+
+###################################################################################################
+# Hydraulic configuration for BaRatin:  RC ==>  Q(h)  =  a* ( h - b ) ^ c  
+###################################################################################################
+# Where:
+# - Q is the discharge,
+# - h is the water level (also called stage)
+# - a,b,c are the RC parameters for which priors are needed !!!
+# Hydraulic configuration:
+ncontrols         = 2   #number of controls
+M                 = matrix(0, ncontrols, ncontrols)  #matrix of controls:
+M[1,]             = c(1,0) # control section (rectangular weir in critical condition)
+M[2,]             = c(0,1) # main control channel (rectangular wide channel in uniform condition)
+# M[3,]             = c(0,1,1) # main control channel + 2 floodways (rectangular channels)
+control.type      = 0  #initialisation
+control.type[1]   = "rect.weir"    
+control.type[2]   = "rect.channel"
+# control.type[3]   = "rect.channel"
+
+
+
+
+# PRIORS: (parameterization b,a,c !!!) for each control   
+#------------------------------------------------------
+#Parameter a:
+#------------
+propagat   = TRUE #[FALSE/TRUE] TRUE = propagate the priors of geom/phys properties for parameter "a"
+a.prior    = c()  # initialize the prior for "a" (mean). If you already have it insert the prior.
+st_a.prior = c()  # initialize the prior for "a" (st.dev). If you already have it insert the prior 
+a.distr    = "LogNormal"  # Prior distribution of RC parameter a ("LogNormal"  suggested).
+# Insert mean (Cr.prior) and stdev (st_Cr.prior) of Gaussian 
+# distribution for each control (in the correct order!).
+# or insert NA if "no control"  
+#**************************************************************
+#if rectangular weir: a = Cr*Bw*sqrt(2*g)
+Cr.prior  = c(0.4, NA);       st_Cr.prior = c(0.05, NA)      # flux coefficient
+g.prior   = c(9.81, NA);      st_g.prior  = c(0.005, NA)     # gravity
+Bw.prior  = c(6, NA);         st_Bw.prior = c(0.75, NA)        # weir width
+#if rectangular channel: a = Ks*S0*Bc
+Bc.prior  = c(NA, 25);        st_Bc.prior = c(NA, 3)         # channel width
+KS.prior  = c(NA, 15);        st_KS.prior = c(NA, 2.5)         # Strickler coeff.
+S0.prior  = c(NA, 0.007);  st_S0.prior = c(NA, 0.0025) # Longitudinal slope
+
+
+
+#Parameter b:
+#------------
+b.distr = "Gaussian"  #"Fix" or "Gaussian" or "Uniform" or "Flat"
+b.prior  = c(0.65, 1.1);    st_b.prior = c(0.075, 0.2)
+
+
+#Parameter c:
+#------------
+c.distr = "Gaussian"  #"Fix" or "Gaussian" or "Uniform" or "Flat"
+c.prior = c(1.5, 1.67);     st_c.prior= c(0.025, 0.025)
+
+
+#Remnant error model:
+#--------------------
+#LINEAR: err = g1+g2*Q   or    CONSTANT: err = g1):
+remnant.err.model = "Linear"    #"Linear" or "Constant"
+g1.prior          = c(0, 1000, 0.1)      #c(min,max, starting point)
+g2.prior          = c(0, 100, 0.1)         #c(min,max, starting point) or c(mean, stdev, starting point) 
+#if model = "Constant" then put FALSE !.
+g1.distr.type     = "Uniform"              #  "Uniform.
+g2.distr.type     = "Uniform"            # "Lognormal" or "Uniform
---
+++ Case_studies/Goudet/Options_Segment_gaugings.R
@@ -0,0 +1,49 @@
+# General options:
+################################################################
+name.folder.results        = "mu_1_BIC"
+prior.mu.segm              = c(0, 1)                   # prior for segments mean "mu" = N(mean, stdev): stdev must be of the Order of gaugings residuals !
+tmin                       = 0                          # mininum distance (in time) between two consecutive change points (it depends on time units!). By default =0
+nSmax                      = 3                          # Maximum number of segments in the series at each iteration
+Nmin                       = 1                          # minimum number of data in a segment ( >=1, by default = 1) 
+criterion                  = "BIC"                      # Criterion for the choice of the model ("AIC","BIC", "DIC", "HQC")
+shift.time.adjustment.type = 2                          # 1 = select always the MAP; 2= select always the largest closest flood peak; 3 = will ask to manually insert a value 
+plot_dates                 = TRUE                      # [TRUE/FALSE] plot or not the date of each detected shift time (if any) on the stage record.
+st_b.prior                 = c(0.075, 0.2)           # Initial stdev of b:
+plot.results.only          = FALSE                   # [FALSE/TRUE] if TRUE no computation is performed. only previous results are plotted.
+deltat_peaks               = 500                        # minimum time lag between major floods (to find all major peaks in the series and adjust the shift times), e.g., 100-500 
+
+
+
+
+
+
+# Other BaM options (mcmc, ...): By default
+################################################
+predictionRC         = TRUE
+predictionQt         = FALSE 
+predictionPrior      = FALSE
+simMCMC              = TRUE
+mcmc.prior           = 10000
+Ncycle.segment       = 1500
+Ncycles.max          = 2500
+Nmcmc.segment        = 100
+Nslim.segment        = 100
+Ncycle.baratin       = 500
+
+
+
+
+
+
+
+# Advanced options  - DO NOT TOUCH if you are not sure!    /!\ /!\
+##################################################################
+save.all.results           =  TRUE
+resid.uncertaint           =  TRUE
+recursive                  =  TRUE
+use.other.seg.method       =  FALSE
+prior.gamma.segm.without.U = c(0, 100)  # c(min, max): uniform distrib. when resid.uncertaint = FALSE
+prior.gamma.segm.with.U    = c(log(0.001), 0.001) # c(meanlog, sdlog): LogNormal distrib. when resid.uncertaint =TRUE
+type.stdev.gamma.baratin   = 1  # type of stdev for the RC structural error:  1  (~U(0, mean gamma)) or 2  (~U(0, mean gamma + 2*stdev gamma)) 
+
+
